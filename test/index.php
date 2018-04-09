61612a75277818f86c04b771f03756fe366e51c6
Lyn <yan252079467@vip.qq.com>
2018-04-09 11:26:40 +0800

    no message

---
+++ test/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyn
+ * Date: 2018/3/5
+ * Time: 20:38
+ */
+
+use \Ooooliu\Im\Im;
+include '../vendor/autoload.php';
+
+//new Im('tencent');
+new \Ooooliu\Im\Im('tencent');
